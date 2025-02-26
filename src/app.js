74b7dd3f26614a452bc43a8d7cfb672a09a98bae
DaniVera-tech <daniss.vera@gmail.com>
2025-02-26 16:07:19 +0000

    modificacion

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// Recorrer cada lista con bucles anidados
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      // Concatenar los valores y añadir el dominio .com
+      let domainName = pronoun[i] + adj[j] + noun[k] + '.com';
+      console.log(domainName); // Mostrar el resultado en la consola
+    }
+  }
+}
